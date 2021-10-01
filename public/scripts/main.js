01f8a0cea4c9fbc9caa0f50329b66ae7101065ef
AllyssonR <42867875+AllyssonR@users.noreply.github.com>
2021-09-30 22:50:51 -0300

    create scripts to modal

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,20 +1,25 @@
 import Modal from "./modal.js";
 const modal = Modal();
 //pegar quando o modal for lido
+const modalTitle = document.querySelector(".modal h2");
+const modalDescription = document.querySelector(".modal p");
+const modalButton = document.querySelector(".modal button");
 const checkButtons = document.querySelectorAll(".actions a.check");
 checkButtons.forEach((button) => {
   //adicionar a escuta
-  button.addEventListener("click", (event) => {
-    modal.open();
-  });
+  button.addEventListener("click", handleClick);
 });
 
 //Pegar quando marcar como lido for clicado
 
 const deleteButton = document.querySelectorAll(".actions a.delete");
-
 deleteButton.forEach((button) => {
-  button.addEventListener("click", (event) => {
-    modal.close();
-  });
+  button.addEventListener("click", (event) => handleClick(event, false));
 });
+
+function handleClick(event, check = true) {
+  const text = check ? "marcar como lida" : "Excluir ";
+  modalTitle.innerHTML = `${text}   essa pergunta ?`;
+  modalDescription.innerHTML = `Tem certeza que deseja ${text} esta pergunta ?`;
+  modal.open();
+}
